cc034e31ed87889f03ff5c0cec307dac3891fab1
Scott Reeser <scott@wysereeser.com>
2015-01-22 18:44:46 -0600

    assign 3 done

--- plot3.R
+++ plot3.R
@@ -2,3 +2,23 @@
 # which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City?
 # Which have seen increases in emissions from 1999–2008? Use the ggplot2 plotting system to make 
 # a plot answer this question.
+
+library(dplyr)
+library(ggplot2)
+
+#load data
+NEI <- readRDS("summarySCC_PM25.rds")
+
+#filtering on Baltimore City
+baltimore.data<-filter(NEI,fips == "24510")
+
+#get our subset and sum by year
+baltimore.year.data<-summarise(group_by(select(baltimore.data,Emissions,type,year),year,type),emissions.total=sum(Emissions))
+
+png(filename="plot3.png",width=480,height=480,bg="transparent")
+#plot it out
+qplot(year,baltimore.year.data$emissions.total, data=baltimore.year.data,color=baltimore.year.data$type, geom=c("point","smooth"))
+dev.off()
+
+# Using the plot, non-road, nonpoint, on-road all have seen decreases. Point is the only one that's
+# seen any kind of an increase. 
